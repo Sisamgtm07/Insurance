1373ef3657667f7ae4c6ad2ed99056543025e7f9
Sisamgtm07 <sisamgautam0001@gmail.com>
2020-10-11 17:07:55 +0545

    Vertical Slider to HOW IT Works Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//Todays Slider
+//Testimonial Slider
 $('.slider').slick({
     slidesToShow: 2,
     slidesToScroll: 1,
@@ -27,3 +27,40 @@ $('.slider').slick({
       },
     ]
   });
+
+
+  //HOW IT WORKS Slider
+$('.slider-work').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  infinite: true,
+  vertical: true,
+  verticalSwiping: true,
+  cssEase: 'linear',
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        vertical: true,
+        verticalSwiping: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+      }
+    },
+  ]
+});
