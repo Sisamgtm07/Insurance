dfe6f52eb6ec6f5c1b92c50b49a5479b35b7dc9b
Sisamgtm07 <sisamgautam0001@gmail.com>
2020-10-16 20:44:23 +0545

    Article Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,3 +94,33 @@ $('.partner-slider').slick({
     },
   ]
 });
+
+//Article Slider
+$('.article-slider').slick({
+  slidesToShow:3,
+  slidesToScroll: 1,
+  dots: true,
+  // autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  cssEase: 'linear',
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+  ]
+});
