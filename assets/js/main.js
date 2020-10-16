c7b0bda84032c5427cc87a2dcc566790868e9ca5
Sisamgtm07 <sisamgautam0001@gmail.com>
2020-10-16 19:42:24 +0545

    Partner Section Slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,4 +65,32 @@ $('.slider-work').slick({
   ]
 });
 
+//Partner Slider
+$('.partner-slider').slick({
+  slidesToShow:6,
+  slidesToScroll: 2,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  cssEase: 'linear',
 
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+  ]
+});
